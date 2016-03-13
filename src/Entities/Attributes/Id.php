2c2fe91c340e34064a16ac3a5f2b847d3d413abd
Jaroslav Hranička <hranicka@outlook.com>
2016-03-13 09:47:27 +0100

    Id added

---
+++ src/Entities/Attributes/Id.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of etten/doctrine.
+ * Copyright © 2016 Jaroslav Hranička <hranicka@outlook.com>
+ */
+
+namespace Etten\Doctrine\Entities\Attributes;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait Id
+{
+
+	/**
+	 * @var int
+	 * @ORM\Id()
+	 * @ORM\Column(type="bigint")
+	 * @ORM\GeneratedValue()
+	 */
+	private $id;
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		if (!$this->id) {
+			throw new \RuntimeException('ID is not set.');
+		}
+
+		return $this->id;
+	}
+
+	public function __clone()
+	{
+		$this->id = NULL;
+	}
+
+}
