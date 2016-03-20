91c81de8ae9f3323f24e8746a971cbd66bd18be7
Jaroslav Hranička <hranicka@outlook.com>
2016-03-20 10:14:00 +0100

    IdProvider added

---
+++ src/Entities/IdProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of etten/doctrine.
+ * Copyright © 2016 Jaroslav Hranička <hranicka@outlook.com>
+ */
+
+namespace Etten\Doctrine\Entities;
+
+interface IdProvider
+{
+
+	public function getId();
+
+}
