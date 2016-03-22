4f632b3cfb149ab51173d6cdec55c936254e16c5
Jaroslav Hranička <hranicka@outlook.com>
2016-03-22 09:52:17 +0100

    Entities\AbstractEntity added

---
+++ src/Entities/AbstractEntity.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of etten/doctrine.
+ * Copyright © 2016 Jaroslav Hranička <hranicka@outlook.com>
+ */
+
+namespace Etten\Doctrine\Entities;
+
+use Doctrine\ORM;
+
+/**
+ * @ORM\Mapping\Entity()
+ */
+abstract class AbstractEntity implements IdProvider, Cacheable
+{
+
+	public function getCacheKey()
+	{
+		return get_called_class() . $this->getId();
+	}
+
+	public function getCacheTags()
+	{
+		$nameParts = explode('\\', get_called_class());
+		$name = strtolower(end($nameParts));
+
+		return [$name];
+	}
+
+}
