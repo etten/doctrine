b6456a9c37fabd96149fab60ea851b214e110bdc
Jaroslav Hranička <hranicka@outlook.com>
2016-05-30 08:11:04 +0200

    Etten\Doctrine\Facade\Facade back-compatibility

---
+++ src/Facade/Facade.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of etten/doctrine.
+ * Copyright © 2016 Jaroslav Hranička <hranicka@outlook.com>
+ */
+
+namespace Etten\Doctrine\Facade;
+
+/**
+ * @deprecated Use Etten\Doctrine\Facade instead.
+ */
+class Facade extends \Etten\Doctrine\Facade
+{
+
+}
