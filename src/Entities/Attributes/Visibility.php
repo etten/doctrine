4d27131db1c6fec53512562362effb9a1099a555
Jaroslav Hranička <hranicka@outlook.com>
2016-06-03 11:24:18 +0200

    Entities\Hideable added

---
+++ src/Entities/Attributes/Visibility.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of etten/doctrine.
+ * Copyright © 2016 Jaroslav Hranička <hranicka@outlook.com>
+ */
+
+namespace Etten\Doctrine\Entities\Attributes;
+
+trait Visibility
+{
+
+	/**
+	 * @var bool
+	 * @ORM\Column(type="boolean")
+	 */
+	protected $visible;
+
+	/**
+	 * @return bool
+	 */
+	public function isVisible():bool
+	{
+		return $this->visible;
+	}
+
+	/**
+	 * @param boolean $visible
+	 * @return $this
+	 */
+	public function setVisible(bool $visible)
+	{
+		$this->visible = $visible;
+		return $this;
+	}
+
+}
---
+++ src/Entities/Hideable.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of etten/doctrine.
+ * Copyright © 2016 Jaroslav Hranička <hranicka@outlook.com>
+ */
+
+namespace Etten\Doctrine\Entities;
+
+interface Hideable
+{
+
+	public function isVisible():bool;
+
+	public function setVisible(bool $visible);
+
+}
