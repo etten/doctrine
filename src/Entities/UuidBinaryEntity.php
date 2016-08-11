351dd1589e40d4cade68d2edff28bd34baf56fa4
Jaroslav Hranička <hranicka@outlook.com>
2016-08-11 15:15:59 +0200

    UuidEntity + UuidBinaryEntity added

---
+++ src/Entities/UuidBinaryEntity.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright © 2016 Jaroslav Hranička <hranicka@outlook.com>
+ */
+
+namespace Etten\Doctrine\Entities;
+
+abstract class UuidBinaryEntity extends Entity
+{
+
+	use Attributes\UuidBinary;
+
+}
